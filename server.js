e8a79a6f1ac782b6d592a7ee1e75c3768231a046
Vaidehi Vaze <vaidehivaze0709@gmail.com>
2026-06-02 11:17:34 +0530

    Added one more route

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get("/", function(req, res){
     res.send("Hello! I am in server.js")
 })
 
+app.get("/home", function(req, res){
+    res.send("I am in Home Page!")
+})
+
 app.listen(3000)
 
 // console.log("Hello!")
